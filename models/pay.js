ba40c8b09e6f2c438500ff12cecf48f618d0f98d
Rowinggwp <145156861+Rowinggwp@users.noreply.github.com>
2024-11-10 14:44:36 -0500

    cambios tontos

--- models/pay.js
+++ models/pay.js
@@ -3,7 +3,7 @@ const { Schema, model } = require('mongoose');
 const PaySchema = Schema({
     // Número de pago, que se incrementará automáticamente
     numberpay: {
-        type: Number,
+        type: String,
         required: true,
         unique: true // Debe ser único
     },
@@ -23,11 +23,11 @@ const PaySchema = Schema({
 });
 
 // Hook para generar un número de pago secuencial
-// PaySchema.pre('save', async function (next) {
-//     const lastPay = await this.constructor.findOne().sort({ numberpay: -1 });
-//     this.numberpay = lastPay ? lastPay.numberpay + 1 : 1; // Si no existe ningún número previo, empieza en 1
-//     next();
-// });
+ PaySchema.pre('save', async function (next) {
+     const lastPay = await this.constructor.findOne().sort({ numberpay: -1 });
+     this.numberpay = lastPay ? lastPay.numberpay + 1 : 1; // Si no existe ningún número previo, empieza en 1
+     next();
+ });
 
 PaySchema.methods.toJSON = function () {
     const { __v, state, ...payObject } = this.toObject();
